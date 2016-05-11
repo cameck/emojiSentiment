1d440fe1945f2250bb89df160c6a64b19795d447
Alex Vera <veraalex97@gmail.com>
2016-05-10 21:45:50 -0400

    Added user authentication, edited routes, sessions controller.

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
