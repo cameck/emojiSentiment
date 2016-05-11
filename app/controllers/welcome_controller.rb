9792055b4aacbfd30cb717716d117a497c840015
Cameron Eckelberry <cameck@gmail.com>
2016-05-10 21:01:13 -0400

    added search field and twitter creds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
+
   def index
+
+  end
+
+  def show
+    render 'index'
+  end
+
+  private
+
+  def clean_params
+    params.permit(:hash_tag)
   end
 end
--- app/models/twitter_bot.rb
+++ app/models/twitter_bot.rb
@@ -1,2 +1,14 @@
 class TwitterBot < ActiveRecord::Base
+  def client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET']
+      config.access_token        = ENV['TWITTER_TOKEN']
+      config.access_token_secret = ENV['TWITTER_T_SECRET']
+    end
+  end
+
+  def search(input)
+    
+  end
 end
