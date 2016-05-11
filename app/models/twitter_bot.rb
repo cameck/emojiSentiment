89b8dd7f8ee9e240218a75637de30d0471745b62
Cameron Eckelberry <cameck@gmail.com>
2016-05-11 19:36:12 -0400

    Added Initial Search Ranking Logic

--- app/models/twitter_bot.rb
+++ app/models/twitter_bot.rb
@@ -10,20 +10,31 @@ class TwitterBot < ActiveRecord::Base
   end
 
   def search(input)
+
+    # GET Twitter hashtag search Results
     search = client.search("##{input}", result_type: "recent").take(1000)
 
+
+    # Scan for Emojis & Save in Array
     tweets = []
 
-    search.each do |search_item|
-
+    search.each_with_index do |search_item, index|
       text = search_item.text
 
-      if text.include?("🍕")
-        tweets << text
+      EmojiData.scan(text).each do |ec|
+         tweets << ec.unified
       end
     end
 
-    return tweets
+    # count unique tweets
+    tweet_counts = Hash.new 0
+    tweets.each do |tweet|
+      tweet_counts[tweet] += 1
+    end
+
+    # sort tweets
+    sorted_tweets = tweet_counts.sort_by {|_key, value| value}
+    sorted_tweets = sorted_tweets.last(3).reverse
 
   end
 end
