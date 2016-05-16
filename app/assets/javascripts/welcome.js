41ac95f568a133832043d96848f3e75ee61f82a8
Cameron Eckelberry <cameck@gmail.com>
2016-05-16 10:19:31 -0400

    Fixed JS for search box

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,9 +1,10 @@
 //Hide and show the search icon on click for the home page search box
 $(document).ready(function(){
   $(".searchfield").focus(function() {
-    console.log("this works!")
       $(this).removeAttr('value');
   }).blur(function() {
-    $(this).attr('value');
-  })
+    var searchIcon = "&#xf002; #";
+    var $searchIcon = $($.parseHTML(searchIcon));
+    $(this).attr('value', $searchIcon.text() );
+  });
 });
