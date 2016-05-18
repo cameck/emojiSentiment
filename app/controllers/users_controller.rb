54ca7d43a00b211e033e55f784df79e24ceecadf
Cameron Eckelberry <cameck@gmail.com>
2016-05-18 18:28:06 -0400

    few more small .nil fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :clean_params, only: [:show]
-
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
@@ -8,22 +8,26 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @saved_searches = saved_searches
-    @saved_search_scores = @saved_searches[0].saved_search_items
+    @saved_search_scores = @saved_searches[0].saved_search_items if @saved_searches
   end
 
   def save_search
-    @saved_searches = saved_searches
-    @saved_search_scores = @saved_searches[0].saved_search_items
-    SavedSearch.create( search: params[:hash_tag], user_id: current_user.id )
-    flash[:notice] = "#{params[:hash_tag]} successfully saved, check back in a few days as we aggregate some data you :)"
+    if current_user
+      @saved_searches = saved_searches
+      @saved_search_scores = @saved_searches[0].saved_search_items if @saved_searches
+      SavedSearch.create( search: params[:hash_tag], user_id: current_user.id )
+      flash[:notice] = "#{params[:hash_tag]} successfully saved, check back in a few days as we aggregate some data you :)"
+    end
     render 'show'
   end
 
   def saved_searches
-    if SavedSearch.where(user_id: current_user.id).blank?
-      "Sorry Nothing here"
-    else
-      SavedSearch.where(user_id: current_user.id)
+    if current_user
+      if SavedSearch.where(user_id: current_user.id) == false
+        nil
+      else
+        SavedSearch.where(user_id: current_user.id)
+      end
     end
   end
 
