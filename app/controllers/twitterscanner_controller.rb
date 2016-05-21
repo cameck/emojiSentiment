e43884cedb18f8921c60407d4022b86c0bb1ac2c
Alex Vera <veraalex97@gmail.com>
2016-05-20 21:31:16 -0400

    Trying out getting the request from the sinatra app

--- app/controllers/twitterscanner_controller.rb
+++ app/controllers/twitterscanner_controller.rb
@@ -7,7 +7,9 @@ skip_before_action :verify_authenticity_token
   end
 
   def response
-    
+    @response = HTTParty.get("http://intense-coast-35405.herokuapp.com/process-request")
+    @http_json = JSON.parse(@response.body)
+    p http_json
   end
 
 end
