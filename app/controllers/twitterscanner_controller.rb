e1ed84ddf58d70f297e9d5fee9354be6e45fe26f
Alex Vera <veraalex97@gmail.com>
2016-05-20 21:38:31 -0400

    editted the twitterscanner controller

--- app/controllers/twitterscanner_controller.rb
+++ app/controllers/twitterscanner_controller.rb
@@ -8,8 +8,7 @@ skip_before_action :verify_authenticity_token
 
   def response
     @response = HTTParty.get("http://intense-coast-35405.herokuapp.com/process-request")
-    @http_json = JSON.parse(@response.body)
-    p http_json
+    p response
   end
 
 end
