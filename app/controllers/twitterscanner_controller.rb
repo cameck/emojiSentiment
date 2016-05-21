747048897db5b11a8dbe9d986a5048b7f54218b4
Cameron Eckelberry <cameck@gmail.com>
2016-05-21 18:07:00 -0400

    fixed bug

--- app/controllers/twitterscanner_controller.rb
+++ app/controllers/twitterscanner_controller.rb
@@ -25,7 +25,7 @@ class TwitterscannerController < ApplicationController
         sentiments = twitter_bot.get_sentiment(tweets)
         tweet_at_user = "@#{user_name} Top 3 emojis & their associated sentiment #{tweets[0][0][0]}=#{sentiments[0]} #{tweets[1][0][0]}=#{sentiments[1]} #{tweets[2][0][0]}=#{sentiments[2]} | More info @ http://bit.ly/1qBqqJv (edited)"
         p "Tweet to send: #{tweet_at_user}"
-        p client.update(tweet_at_user)
+        p twitter_bot.client.update(tweet_at_user)
       end
     # rescue
     #   render :nothing => true, :status => 500
