43367ddb99f3120145047361da788b61cf6ff4c7
Cameron Eckelberry <cameck@gmail.com>
2016-05-22 12:48:23 -0400

    Small Verbiage change

--- app/controllers/twitterscanner_controller.rb
+++ app/controllers/twitterscanner_controller.rb
@@ -9,7 +9,7 @@ class TwitterscannerController < ApplicationController
 
       if tweets
         sentiments = twitter_bot.get_sentiment(tweets)
-        tweet_at_user = "@#{user_tweet_data[:user_name]} Top 3 emoji sentiment score for #{user_tweet_data[:hashtag]}: #{tweets[0][0][0]}=#{sentiments[0]} #{tweets[1][0][0]}=#{sentiments[1]} #{tweets[2][0][0]}=#{sentiments[2]} | Details @ http://bit.ly/1qBqqJv"
+        tweet_at_user = "@#{user_tweet_data[:user_name]} Top 3 emoji & sentiment score for #{user_tweet_data[:hashtag]}: #{tweets[0][0][0]}=#{sentiments[0]} #{tweets[1][0][0]}=#{sentiments[1]} #{tweets[2][0][0]}=#{sentiments[2]} | Details @ http://bit.ly/1qBqqJv"
 
         twitter_bot.client.update(tweet_at_user)
       end
