632cd3dacf0ed261dbc20b3f32df2e1143b060ec
Cameron Eckelberry <cameck@gmail.com>
2016-05-22 16:45:03 -0400

    Moved to S3 image hosting

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,6 @@
 //Hide and show the search icon on click for the home page search box
 $(document).ready(function(){
-
+  // Add the Anchor Link home so Full page JS works with Back Button
   window.location.hash = "#Home"
   // Initialize Full Page JS
   $('#fullpage').fullpage({
